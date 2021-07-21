097ae6b2ab4e8c41b1742f2c930399a00425d0c3
Victoria Kazeem <victoria.kazeem@pmglobaltechnology.com>
2021-07-21 22:33:41 +0100

    Push changes

--- models/MinistryArm.js
+++ models/MinistryArm.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MinistryArmSchema = new Schema({
-
     date_created: {
         type: Date,
         default: Date.now,
@@ -28,11 +27,19 @@ const MinistryArmSchema = new Schema({
         type: String,
         required: true,
     },
+    directorate_details: {
+        type: Object,
+        required: true
+    },
 
     ministry_head: {
         type: String,
         required: true,
     },
+    ministry_head_details: {
+        type: Object,
+        required: true
+    }
 });
 
 module.exports = MinistryArm = mongoose.model("ministry_arm", MinistryArmSchema);
