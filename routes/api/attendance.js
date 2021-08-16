943da0df0cecdf1dafc0803bec6537af7bf0f294
Victoria Kazeem <victoria.kazeem@pmglobaltechnology.com>
2021-08-17 00:33:33 +0100

    Push changes

--- routes/api/attendance.js
+++ routes/api/attendance.js
@@ -104,14 +104,16 @@ router.post("/", auth, (req, res) => {
 
     Attendance.findOne({ worker_id }).then((attendance) => {
         let today = new Date().toISOString().split("T")[0];
-        console.log("attendance.date_created", attendance.date_created);
-        if (attendance && attendance.date_created.split("T")[0] === today) {
-            return res.status(400).json({
-                msg: "Worker has already been marked present!",
-            });
+
+        if (attendance) {
+            console.log("attendance.date_created", attendance.date_created);
+            if (attendance.date_created.split("T")[0] === today) {
+                return res.status(400).json({
+                    msg: "Worker has already been marked present!",
+                });
+            }
+
         }
-
-
 
         const newAttendance = new Attendance({
             date_created, user_id, worker_id, worker_details
